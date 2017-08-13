aa9d636ce3899976b7700cf3ca857b83a2333f07
Findley, Randy <rgfindley@gmail.com>
2017-08-12 22:32:43 -0400

    Update tests.  Test build.

--- test/test.js
+++ test/test.js
@@ -5,8 +5,13 @@ var index = require('../index');
 
 describe('index', function() {
     it('main', function (done) {
-        index.handler({}, {}, function(err, response) {
+        index.handler({test:true}, {}, function(err, response) {
             assert.equal(response.statusCode, 200);
+            assert.ok(response.body);
+            var body = JSON.parse(response.body);
+            assert.ok(body.event);
+            assert.ok(body.event.test);
+            winston.info(JSON.stringify(response, null, 3));
             done();
         });
     });
