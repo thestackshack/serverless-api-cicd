d86070053a60d19d397e9a63d2a5d6d2cfbd903e
Findley, Randy <rgfindley@gmail.com>
2017-08-14 06:50:49 -0400

    Improve package lambda

---
+++ iac/package.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var AWS = require('aws-sdk');
+
+exports.handler = function(event, context) {
+
+    console.log(JSON.stringify(event, null, 3));
+    console.log(JSON.stringify(event));
+
+    var codepipeline = new AWS.CodePipeline();
+
+    // Retrieve the Job ID from the Lambda action
+    var jobId = event["CodePipeline.job"].id;
+
+    // Notify AWS CodePipeline of a successful job
+    var putJobSuccess = function(message) {
+        var params = {
+            jobId: jobId
+        };
+        codepipeline.putJobSuccessResult(params, function(err, data) {
+            if(err) {
+                context.fail(err);
+            } else {
+                context.succeed(message);
+            }
+        });
+    };
+
+    // Notify AWS CodePipeline of a failed job
+    var putJobFailure = function(message) {
+        var params = {
+            jobId: jobId,
+            failureDetails: {
+                message: JSON.stringify(message),
+                type: 'JobFailed',
+                externalExecutionId: context.invokeid
+            }
+        };
+        codepipeline.putJobFailureResult(params, function(err, data) {
+            context.fail(message);
+        });
+    };
+
+    // Succeed the job
+    putJobSuccess("Packaged successfully.");
+};
